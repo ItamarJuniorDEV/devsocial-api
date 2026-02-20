fa65f650a5e16a4a0eec9f96d769fa070aaae363
Itamar Junior <cdajuniorf@gmail.com>
2026-02-20 12:33:27 -0300

    feat: PostPolicy com like, comment, update e delete

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Post;
+use App\Models\User;
+
+class PostPolicy
+{
+    public function like(User $user, Post $post): bool
+    {
+        return true;
+    }
+
+    public function comment(User $user, Post $post): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Post $post): bool
+    {
+        return $user->id === $post->user_id;
+    }
+
+    public function delete(User $user, Post $post): bool
+    {
+        return $user->id === $post->user_id;
+    }
+}
