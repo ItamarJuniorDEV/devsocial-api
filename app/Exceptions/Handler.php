a503b7e45c521d91f8e2545be77d280d097412e0
Itamar Junior <cdajuniorf@gmail.com>
2026-02-20 12:30:30 -0300

    feat: bootstrap, kernel, configs e infraestrutura base do Laravel

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    public function register(): void
+    {
+        $this->renderable(function (BaseApiException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], $e->getStatus());
+        });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json(['message' => 'Unauthenticated.'], 401);
+    }
+
+    protected function invalidJson($request, ValidationException $exception)
+    {
+        return response()->json([
+            'message' => $exception->getMessage(),
+            'errors'  => $exception->errors(),
+        ], $exception->status);
+    }
+}
